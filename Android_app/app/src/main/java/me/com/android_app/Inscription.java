5ca133da204c3bac1a3fe116af7c6c645f38ac91
Speedy engineer <laurentlefort.etudiant@gmail.com>
2015-04-18 14:43:37 +0200

    app.Activity

--- Android_app/app/src/main/java/me/com/android_app/Inscription.java
+++ Android_app/app/src/main/java/me/com/android_app/Inscription.java
@@ -1,6 +1,6 @@
 package me.com.android_app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
