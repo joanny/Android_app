d143e38dfcf1678dfd6abd45cc5fa6e54967d5f8
Speedy engineer <laurentlefort.etudiant@gmail.com>
2015-04-19 01:37:57 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	application/.idea/misc.xml

---
+++ application/app/src/main/java/com/example/myapplication/AdapterMaker.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by mekinconnu on 18/04/2015.
+ */
+public class AdapterMaker {
+
+    AdapterMaker(){
+
+    }
+    static void setAdapter(int array, Context context, Spinner spinner){
+
+        spinner.setAdapter(new ArrayAdapter<String>(context,android.R.layout.simple_spinner_item,context.getResources().getStringArray(array)));
+    }
+}
---
+++ application/app/src/main/java/com/example/myapplication/InscriptionController.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+/**
+ * Created by mekinconnu on 19/04/2015.
+ */
+public class InscriptionController {
+}
